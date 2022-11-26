d2927d69f32d019e9ad37e7f15deed6fc11e4022
yuutan1017 <n.you0611@gmail.com>
2022-11-26 20:50:56 +0900

    next:firebase setting

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 
+const addModal = document.getElementById('add');
+const btnAdd = document.querySelector('.btn-add');
+
+btnAdd.addEventListener('click', () => {
+  addModal.style.display = 'block';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target == addModal) {
+    addModal.style.display = 'none';
+  }
+});
 
